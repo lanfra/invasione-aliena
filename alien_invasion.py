604df18431065184f72f770338a8e4de56a80a50
Lanfra <lanfranco.giorgetti@gmail.com>
2026-02-26 23:59:58 +0100

    Aggiunto colore di sfondo e frequenza di aggiornamento videata 60 volte al secondo

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,10 +7,14 @@ class AlienInvasion:
     def __init__(self):
         """Risorse del gioco."""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         #Finestra setting.
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Invasione Aliena")
+
+        #Colore di sfondo.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Main Avvio gioco."""
@@ -20,8 +24,12 @@ class AlienInvasion:
                 if event.type == pygame.quit:
                     sys.exit()
 
+                #Colora schermata di sfondo.
+                self.screen.fill(self.bg_color)
                 #Mostra finestra.
                 pygame.display.flip()
+                #Frequenza di aggiornamento schermata.
+                self.clock.tick(60) #Frequenza di aggiornamento di volte 60 al secondo.
 
 #Avvio gioco.
 if __name__ == "__main__":
